9cfe2fbd8630e7568a942c38bd7341ab63fdcdc0
ABHISHEK SHAW <56201731+Abhishekshaw29@users.noreply.github.com>
2021-06-25 11:15:00 +0530

    enter

--- src/main/java/com/springrest/springrest/controller/MyController.java
+++ src/main/java/com/springrest/springrest/controller/MyController.java
@@ -7,6 +7,8 @@ import com.springrest.springrest.entity.Course;
 import com.springrest.springrest.services.Courseservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,10 +28,10 @@ public class MyController {
             return "HELLO WORLD";
         }
 
-    @GetMapping("/home")
-    public String demo(){
-        return "Welcome to home";
-    }
+    // @GetMapping("/home")
+    // public String demo(){
+    //     return "Welcome to home";
+    // }
 
     //get all the courses
     @GetMapping("/Courses")
@@ -38,18 +40,21 @@ public class MyController {
         return this.courseservice.getCourses();
         
     }
-
+    
+    //get course by id
     @GetMapping("/Courses/{Course_id}")
     public Course getCourse(@PathVariable String Course_id){
         return this.courseservice.getCourse(Long.parseLong(Course_id));
     }
 
+    //add new courses by sending json
     //can write consumes json.application.
     @PostMapping("/Courses")
     public Course addCourse(@RequestBody Course course){
         return this.courseservice.addCourse(course);
     }
 
+    //update all the course
     @PutMapping("/Courses/{Course_id}")
     public Course updateCourse(@RequestBody Course course , @PathVariable String Course_id){
         
@@ -57,9 +62,20 @@ public class MyController {
         return course;
     }
     
+    //delete the course
     @DeleteMapping("/Courses/{Course_id}")
-    public Course deleteCourse(@PathVariable String Course_id){
-        return this.courseservice.deleteCourse(Long.parseLong (Course_id));
+    public ResponseEntity<HttpStatus> deleteCourse(@PathVariable String Course_id){
+        //send https status using response entity
+        try {
+            this.courseservice.deleteCourse(Long.parseLong (Course_id));
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            //TODO: handle exception
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
+       
     } 
 
 }
